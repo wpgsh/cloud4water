fabfb06873628c54896e2c5d3573aed03d13b4de
Alexander Lukichev <alexander.s.lukichev@gmail.com>
2016-10-25 19:06:56 -0500

    sample equals/hashCode code for composite primary keys

--- apps/wemp/bundles/net.wapwag.wemp.dao/src/main/java/net/wapwag/wemp/dao/model/ObjectOrganizationScope.java
+++ apps/wemp/bundles/net.wapwag.wemp.dao/src/main/java/net/wapwag/wemp/dao/model/ObjectOrganizationScope.java
@@ -16,11 +16,85 @@ public class ObjectOrganizationScope {
 	@Embeddable
 	public static class ObjectOrganizationScopeId implements Serializable {
 	
+		private static final long serialVersionUID = 1L;
+
 		@ManyToOne(fetch=FetchType.EAGER)
 		protected ObjectEntity object;
 		
 		@ManyToOne(fetch=FetchType.EAGER)
 		protected Organization organization;
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((object == null) ? 0 : Long.hashCode(object.getId()));
+			result = prime * result + ((organization == null) ? 0 : Long.hashCode(organization.getId()));
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ObjectOrganizationScopeId other = (ObjectOrganizationScopeId) obj;
+			if (object == null) {
+				if (other.object != null)
+					return false;
+			} else {
+				if (other.object == null) {
+					return false;
+				} else {
+					if (object.getId() != other.object.getId()) {
+						return false;
+					}
+				}
+			}
+			if (organization == null) {
+				if (other.organization != null)
+					return false;
+			} else {
+				if (other.organization == null) {
+					return false;
+				} else {
+					if (organization.getId() != other.organization.getId()) {
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+		
+		public ObjectOrganizationScopeId() {
+			
+		}
+
+		public ObjectOrganizationScopeId(ObjectEntity object, Organization organization) {
+			super();
+			this.object = object;
+			this.organization = organization;
+		}
+
+		public ObjectEntity getObject() {
+			return object;
+		}
+
+		public void setObject(ObjectEntity object) {
+			this.object = object;
+		}
+
+		public Organization getOrganization() {
+			return organization;
+		}
+
+		public void setOrganization(Organization organization) {
+			this.organization = organization;
+		}
 		
 	}
 
--- apps/wemp/bundles/net.wapwag.wemp.dao/src/main/java/net/wapwag/wemp/dao/model/UserOrganizationRole.java
+++ apps/wemp/bundles/net.wapwag.wemp.dao/src/main/java/net/wapwag/wemp/dao/model/UserOrganizationRole.java
@@ -17,11 +17,88 @@ public class UserOrganizationRole {
 	@Embeddable
 	public static class UserOrganizationId implements Serializable {
 				
+		private static final long serialVersionUID = 1L;
+
 		@ManyToOne(fetch=FetchType.EAGER)
 		protected User user;
 		
 		@ManyToOne(fetch=FetchType.EAGER)
 		protected Organization organization;
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((organization == null) ? 0 : Long.hashCode(organization.getId()));
+			result = prime * result + ((user == null) ? 0 : Long.hashCode(user.getId()));
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			UserOrganizationId other = (UserOrganizationId) obj;
+			if (organization == null) {
+				if (other.organization != null)
+					return false;
+			} else {
+				if (other.organization == null) {
+					return false;
+				} else {
+					if (organization.getId() != other.organization.getId()) {
+						return false;
+					}
+				}
+			}
+				
+			if (user == null) {
+				if (other.user != null)
+					return false;
+			} else {
+				if (other.user == null) {
+					return false;
+				} else {
+					if (user.getId() != other.user.getId()) {
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+		
+		public UserOrganizationId() {
+			
+		}
+
+		public UserOrganizationId(User user, Organization organization) {
+			super();
+			this.user = user;
+			this.organization = organization;
+		}
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+		public Organization getOrganization() {
+			return organization;
+		}
+
+		public void setOrganization(Organization organization) {
+			this.organization = organization;
+		}
+		
+		
 		
 	}
 	
