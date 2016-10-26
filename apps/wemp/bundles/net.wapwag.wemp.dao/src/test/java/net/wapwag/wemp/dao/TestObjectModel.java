fdcbd97b900d5cfddb812dbcda182a214eeb2896
asd1245dss <huoyexiangyu@gmail.com>
2016-10-26 15:29:17 +0800

    Refactor test method

--- apps/wemp/bundles/net.wapwag.wemp.dao/src/test/java/net/wapwag/wemp/dao/TestObjectModel.java
+++ apps/wemp/bundles/net.wapwag.wemp.dao/src/test/java/net/wapwag/wemp/dao/TestObjectModel.java
@@ -1,11 +1,13 @@
 package net.wapwag.wemp.dao;
 
-import net.wapwag.wemp.dao.model.geo.Country;
 import net.wapwag.wemp.dao.model.ObjectData;
+import net.wapwag.wemp.dao.model.geo.Country;
+import net.wapwag.wemp.dao.model.link.UserOrg;
+import net.wapwag.wemp.dao.model.link.UserOrgId;
+import net.wapwag.wemp.dao.model.org.Organization;
+import net.wapwag.wemp.dao.model.org.WaterManageAuth;
 import net.wapwag.wemp.dao.model.permission.User;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
@@ -14,19 +16,63 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TestObjectModel {
-	
-	private EntityManager em;
 
-	@Before
-	public void before() {				
+    private static EntityManager em;
+
+    private EntityTransaction tx;
+
+	@BeforeClass
+	public static void before() {
 		em = Persistence.createEntityManagerFactory("test-wemp").createEntityManager();
 	}
 
-	@Test
+    @AfterClass
+    public static void after() {
+        em.close();
+    }
+
+    @Before
+    public void beforeMethod() {
+        tx = em.getTransaction();
+        tx.begin();
+    }
+
+    @Test
+    public void testManyToManyInsert() {
+
+        int i;
+        User user = null;
+
+        for (i = 0; i < 10; i++) {
+            user = new User();
+            user.setName("admin" + i);
+            em.persist(user);
+        }
+
+        Organization organization;
+        Set<Organization> orgSet = new HashSet<>();
+        for (i = 0; i < 10; i++) {
+            organization = new WaterManageAuth();
+            organization.setName("研发部");
+            orgSet.add(organization);
+            em.persist(organization);
+        }
+
+        UserOrg userOrg;
+
+        for (Organization tmp : orgSet) {
+            userOrg = new UserOrg();
+            userOrg.setUserOrgId(new UserOrgId(user, tmp));
+            em.persist(userOrg);
+        }
+
+        tx.commit();
+    }
+
+
+    @Test
 	public void testModel() {
 		
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
         User user = new User();
         user.setName("管理员");
 		em.persist(user);
@@ -52,9 +98,4 @@ public class TestObjectModel {
 		
 	}
 	
-	@After
-	public void after() {
-		em.close();
-	}
-
 }
