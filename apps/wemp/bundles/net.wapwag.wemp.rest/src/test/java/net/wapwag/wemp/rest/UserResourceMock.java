a88ffdf46e12ac0f09d99be40da7afd18ea32b06
asd1245dss <huoyexiangyu@gmail.com>
2016-11-08 08:33:35 +0800

    Use static mock service to export the mock object to the test case

--- apps/wemp/bundles/net.wapwag.wemp.rest/src/test/java/net/wapwag/wemp/rest/UserResourceMock.java
+++ apps/wemp/bundles/net.wapwag.wemp.rest/src/test/java/net/wapwag/wemp/rest/UserResourceMock.java
@@ -1,17 +1,8 @@
 package net.wapwag.wemp.rest;
 
 import net.wapwag.wemp.WaterEquipmentService;
-import net.wapwag.wemp.WaterEquipmentServiceException;
-import net.wapwag.wemp.model.GroupView;
-import net.wapwag.wemp.model.ObjectView;
-import net.wapwag.wemp.model.ResultView;
-import net.wapwag.wemp.model.UserView;
 
-import java.util.stream.Collectors;
-
-import static net.wapwag.wemp.rest.MockData.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * user resource mock
@@ -23,13 +14,6 @@ class UserResourceMock extends UserResource {
 
     public UserResourceMock() {
         waterEquipmentService = mockService;
-
-        try {
-            when(waterEquipmentService.checkPermission(userId, objectData))
-                    .thenReturn(ResultView.newInstance(true));
-        } catch (WaterEquipmentServiceException e) {
-            e.printStackTrace();
-        }
     }
 
 }
--- apps/wemp/bundles/net.wapwag.wemp.rest/src/test/java/net/wapwag/wemp/rest/UserResourceTest.java
+++ apps/wemp/bundles/net.wapwag.wemp.rest/src/test/java/net/wapwag/wemp/rest/UserResourceTest.java
@@ -2,30 +2,27 @@ package net.wapwag.wemp.rest;
 
 import com.google.gson.reflect.TypeToken;
 import net.wapwag.wemp.WaterEquipmentServiceException;
-import net.wapwag.wemp.dao.WaterEquipmentDaoException;
 import net.wapwag.wemp.dao.model.ObjectData;
-import net.wapwag.wemp.model.GroupView;
 import net.wapwag.wemp.model.ObjectView;
 import net.wapwag.wemp.model.ResultView;
-import net.wapwag.wemp.model.UserView;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.Test;
 
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.lang.reflect.Type;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static net.wapwag.wemp.rest.MockData.*;
-import static net.wapwag.wemp.rest.MockData.objId;
-import static org.eclipse.jetty.http.HttpStatus.*;
+import static net.wapwag.wemp.rest.UserResourceMock.mockService;
+import static org.eclipse.jetty.http.HttpStatus.INTERNAL_SERVER_ERROR_500;
+import static org.eclipse.jetty.http.HttpStatus.OK_200;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 /**
@@ -43,32 +40,77 @@ public class UserResourceTest extends BaseResourceTest {
 
     @Test
     public void testCheckPermission_True() throws Exception {
+        when(mockService.checkPermission(anyLong(), any(ObjectData.class)))
+                .thenReturn(ResultView.newInstance(true));
+
         path = String.format("/user/%s/checkPermissions", userId);
 
-        Response response = target(path).request().post(Entity.form(new Form().param("objId", objId + "")));
+        Entity<ObjectData> entity = Entity.entity(objectData, MediaType.APPLICATION_JSON);
+
+        Response response = target(path).request().post(entity);
 
         ResultView resultView = getResult(response, ResultView.class);
 
+        assertEquals(OK_200, response.getStatus());
         assertNotNull(resultView);
         assertEquals(1, resultView.count);
     }
 
-//    @Test
-//    public void testGetObjectsByUser() throws Exception {
-//        when(waterEquipmentDao.getObjectsByUser(userId, "read")).thenReturn(new HashSet<>(objectDataList));
-//
-//        Set<ObjectView> objectViews = waterEquipmentService.getObjectsByUser(userId, "read");
-//
-//        assertNotNull(objectViews);
-//        assertEquals(objectDataList.size(), objectViews.size());
-//    }
-//
-//    @Test
-//    public void testGetObjectsByUser_Exception() throws Exception {
-//        when(waterEquipmentDao.getObjectsByUser(userId, "read")).thenThrow(WaterEquipmentDaoException.class);
-//
-//        Set<ObjectView> objectViews = waterEquipmentService.getObjectsByUser(userId, "read");
-//
-//        assertNull(objectViews);
-//    }
+    @Test
+    public void testCheckPermission_False() throws Exception {
+        when(mockService.checkPermission(anyLong(), any(ObjectData.class)))
+                .thenReturn(ResultView.newInstance(false));
+
+        path = String.format("/user/%s/checkPermissions", userId);
+
+        Entity<ObjectData> entity = Entity.entity(objectData, MediaType.APPLICATION_JSON);
+
+        Response response = target(path).request().post(entity);
+
+        ResultView resultView = getResult(response, ResultView.class);
+
+        assertEquals(OK_200, response.getStatus());
+        assertNotNull(resultView);
+        assertEquals(0, resultView.count);
+    }
+
+    @Test
+    public void testCheckPermission_Exception() throws Exception {
+        when(mockService.checkPermission(anyLong(), any(ObjectData.class))).thenThrow(WaterEquipmentServiceException.class);
+
+        path = String.format("/user/%s/checkPermissions", userId);
+
+        Entity<ObjectData> entity = Entity.entity(objectData, MediaType.APPLICATION_JSON);
+
+        Response response = target(path).request().post(entity);
+
+        assertEquals(INTERNAL_SERVER_ERROR_500, response.getStatus());
+    }
+
+    @Test
+    public void testGetObjectsByUser() throws Exception {
+        when(mockService.getObjectsByUser(userId, "read"))
+                .thenReturn(objectDataList.stream().map(ObjectView::newInstance).collect(Collectors.toSet()));
+
+        path = String.format("/user/%s/objects", userId);
+
+        Response response = target(path).queryParam("action", action).request().get();
+        Type type = new TypeToken<Set<ObjectView>>(){}.getType();
+        Set<ObjectView> objectViews = getResult(response, type);
+
+        assertEquals(OK_200, response.getStatus());
+        assertNotNull(objectViews);
+        assertEquals(objectDataList.size(), objectViews.size());
+    }
+
+    @Test
+    public void testGetObjectsByUser_Exception() throws Exception {
+        when(mockService.getObjectsByUser(userId, "read")).thenThrow(WaterEquipmentServiceException.class);
+
+        path = String.format("/user/%s/objects", userId);
+
+        Response response = target(path).queryParam("action", action).request().get();
+
+        assertEquals(INTERNAL_SERVER_ERROR_500, response.getStatus());
+    }
 }
