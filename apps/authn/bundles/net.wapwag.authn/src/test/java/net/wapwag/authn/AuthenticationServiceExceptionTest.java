33a8d08703198b1179b06af322472b086bf3654b
sdwbvios <jiangzehu@163.com>
2016-11-10 16:33:30 +0800

    AuthenticationServiceExceptionTest SUBMIT

--- apps/authn/bundles/net.wapwag.authn/src/test/java/net/wapwag/authn/AuthenticationServiceExceptionTest.java
+++ apps/authn/bundles/net.wapwag.authn/src/test/java/net/wapwag/authn/AuthenticationServiceExceptionTest.java
@@ -8,147 +8,26 @@ public class AuthenticationServiceExceptionTest {
 
 	@Test
 	public void testAuthenticationServiceException() {
-		fail("Not yet implemented");
+		AuthenticationServiceException authenticationServiceException = new AuthenticationServiceException();
+		assertNotNull(authenticationServiceException);
 	}
 
 	@Test
 	public void testAuthenticationServiceExceptionString() {
-		fail("Not yet implemented");
+		AuthenticationServiceException authenticationServiceException = new AuthenticationServiceException("Exception");
+		assertNotNull(authenticationServiceException);
 	}
 
 	@Test
 	public void testAuthenticationServiceExceptionThrowable() {
-		fail("Not yet implemented");
+		AuthenticationServiceException authenticationServiceException = new AuthenticationServiceException(new NullPointerException());
+		assertNotNull(authenticationServiceException);
 	}
 
 	@Test
 	public void testAuthenticationServiceExceptionStringThrowable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testException() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExceptionString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExceptionStringThrowable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExceptionThrowable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExceptionStringThrowableBooleanBoolean() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testThrowable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testThrowableString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testThrowableStringThrowable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testThrowableThrowable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testThrowableStringThrowableBooleanBoolean() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetMessage() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetLocalizedMessage() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCause() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testInitCause() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPrintStackTrace() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPrintStackTracePrintStream() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPrintStackTracePrintWriter() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFillInStackTrace() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetStackTrace() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetStackTrace() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetStackTraceDepth() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetStackTraceElement() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddSuppressed() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetSuppressed() {
-		fail("Not yet implemented");
+		AuthenticationServiceException authenticationServiceException = new AuthenticationServiceException("Exception", new NullPointerException());
+		assertNotNull(authenticationServiceException);
 	}
 
 }
