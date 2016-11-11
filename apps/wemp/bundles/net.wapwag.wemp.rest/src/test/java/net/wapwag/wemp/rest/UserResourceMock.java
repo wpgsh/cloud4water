8f89810e6b16dd418a0b2746d759c8e301a33bbf
Alexander Lukichev <alexander.s.lukichev@gmail.com>
2016-11-11 16:48:50 -0600

    add missing resource-level OAuth2 annotation to UserResourceMock

--- apps/wemp/bundles/net.wapwag.wemp.rest/src/test/java/net/wapwag/wemp/rest/UserResourceMock.java
+++ apps/wemp/bundles/net.wapwag.wemp.rest/src/test/java/net/wapwag/wemp/rest/UserResourceMock.java
@@ -1,13 +1,17 @@
 package net.wapwag.wemp.rest;
 
 import net.wapwag.wemp.WaterEquipmentService;
+import net.wapwag.wemp.rest.oauth2.WempTokenHandler;
 
 import static org.mockito.Mockito.mock;
+
+import com.thingswise.appframework.jaxrs.utils.OAuth2;
 
 /**
  * user resource mock
  * Created by Administrator on 2016/11/7 0007.
  */
+@OAuth2(tokenHandler = WempTokenHandler.NAME)
 class UserResourceMock extends UserResource {
 
     static WaterEquipmentService mockService = mock(WaterEquipmentService.class);
